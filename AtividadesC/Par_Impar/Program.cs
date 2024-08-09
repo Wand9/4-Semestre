3c1de90fb595c01b4b0a07aeaccc3be0cba7bdcc
Wanderson Cesar Bonfim Da Silva <48599214888@info.local>
2024-08-09 10:49:13 -0300

    Incluindo atividades de revisao

---
+++ AtividadesC/Par_Impar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ParOuImpar
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número inteiro: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+
+        Console.ReadKey();
+    }
+}
